dde82ca79d715e696c038abe9c791979ec9eca5f
OptoCloud <26223094+OptoCloud@users.noreply.github.com>
2021-04-30 21:56:05 +0200

    Made some variables more readable, scoped variables, and removed useless stuff

--- mapper.js
+++ mapper.js
@@ -2,29 +2,21 @@ const fs = require('fs');
 const OJS = require('owop-js');
 const PNG = require('pngjs').PNG;
 
-// Disregard the two first aguments, theire stupid
-var argc = process.argv.length - 2;
-if (argc != 5) {
+if (process.argv.length != 7) {
     showUsage();
 }
 
-// The arguments that actually matter
-var argv = process.argv.slice(2);
-
-// Parse all other args except the first one because that the server name
-var argv_int = parseArgsAsInts(argv.slice(1), argc);
+const worldName = process.argv[2];
+const pixelRootX = Math.floor(Number(process.argv[3]));
+const pixelRootY = Math.floor(Number(process.argv[4]));
+const imageHeight = Math.floor(Number(process.argv[5]));
+const imageWidth  = Math.floor(Number(process.argv[6]));
 
 // Ur stupid if u get this error, images cant be negative in size
-if (argv_int[2] <= 0 || argv_int[3] <= 0) {
+if (imageHeight <= 0 || imageWidth <= 0) {
     console.error('Image height/width cannot be negative!');
     showUsage();
 }
-
-// Declare a bunch of constants that imma need later
-const root_x = argv_int[0];
-const root_y = argv_int[1];
-const imageHeight = argv_int[2];
-const imageWidth  = argv_int[3];
 
 // Should be 16x16
 const chunkSize = OJS.Client.options.chunkSize;
@@ -35,8 +27,8 @@ const imageChunksH = Math.floor(imageWidth / chunkSize);
 const imageChunksTotal = imageChunksH * imageChunksV;
 
 // Image root coordinates in chunk positions
-const root_cx = Math.floor(root_x / chunkSize);
-const root_cy = Math.floor(root_y / chunkSize);
+const chunkRootX = Math.floor(pixelRootX / chunkSize);
+const chunkRootY = Math.floor(pixelRootY / chunkSize);
 
 // How many chunks we have drawn to the canvas
 var readChunks = 0;
@@ -56,7 +48,7 @@ var canvas = new PNG({
 var client = new OJS.Client({
     reconnect: true,
     controller: true,
-    world: argv[0]
+    world: worldName
 });
 
 client.once('join', start);
@@ -69,10 +61,16 @@ async function start() {
     requestChunks();
     await awaitFinishedCanvas();
 
-    var filename = 'screenshots/canvas_' + imageHeight + 'x' + imageWidth + '_' + root_x + '-' + root_y + '.png';
+    let dirname = 'renders';
+
+    if (!fs.existsSync(dirname)){
+        fs.mkdirSync(dirname);
+    }
+
+    let filename = worldName + '_' + imageHeight + 'x' + imageWidth + '_' + pixelRootX + '-' + pixelRootY + '_';
 
     console.log('\nSaving canvas as "' + filename + '"');
-    fs.writeFileSync(filename, PNG.sync.write(canvas, { colorType: 2 }));
+    fs.writeFileSync(dirname + '/' + filename + '.png', PNG.sync.write(canvas, { colorType: 2 }));
 
     console.log('Done!');
     process.exit(0);
@@ -81,14 +79,14 @@ async function start() {
 // Request all the chunks in our target area
 async function requestChunks() {
     while (!checkAllDrawn()) {
-        for (var cy = 0; cy < imageChunksV; ++cy) {
-            for (var cx = 0; cx < imageChunksH; ++cx) {
+        for (let cy = 0; cy < imageChunksV; ++cy) {
+            for (let cx = 0; cx < imageChunksH; ++cx) {
                 if (shouldDrawChunk(cx, cy)) {
                     try {
                         // If we are disconnected from the map, then wait until we reconnect
                         while(!client.net.isWebsocketConnected || !client.net.isWorldConnected) await timeout(5);
 
-                        client.world.requestChunk(root_cx + cx, root_cy + cy).catch(err => { --cx; });
+                        client.world.requestChunk(chunkRootX + cx, chunkRootY + cy).catch(err => { --cx; });
 
                         // We dont wanna be rate limited
                         await timeout(1);
@@ -109,28 +107,28 @@ async function awaitFinishedCanvas() {
 // This paints chunks to the canvas
 async function paintChunk(cx, cy, raw, protected) {
     // Stupid stuff to avoid re-drawing chunks
-    if (shouldDrawChunk(cx - root_cx, cy - root_cy)) {
+    if (shouldDrawChunk(cx - chunkRootX, cy - chunkRootY)) {
 
         // Index of chunk array
-        var idx = 0;
+        let idx = 0;
 
         // Get chunk pixel-position
-        var cpx = (cx * chunkSize) - root_x;
-        var cpy = (cy * chunkSize) - root_y;
+        let cpx = (cx * chunkSize) - pixelRootX;
+        let cpy = (cy * chunkSize) - pixelRootY;
 
         // For every pixel of the chunk, draw them to the canvas at a calculated offset
-        for (var py = 0; py < chunkSize; ++py) {
-            for (var px = 0; px < chunkSize; ++px) {
-                var r = raw[idx++];
-                var g = raw[idx++];
-                var b = raw[idx++];
+        for (let py = 0; py < chunkSize; ++py) {
+            for (let px = 0; px < chunkSize; ++px) {
+                let r = raw[idx++];
+                let g = raw[idx++];
+                let b = raw[idx++];
 
                 setPixel(r, g, b, cpx + px, cpy + py);
             }
         }
 
         // Register the current chunk as drawn
-        setChunkDrawn(cx - root_cx, cy - root_cy);
+        setChunkDrawn(cx - chunkRootX, cy - chunkRootY);
 
         // Increment the amount of read chunks, and print the current progress
         printProgress(++readChunks / imageChunksTotal);
@@ -139,12 +137,12 @@ async function paintChunk(cx, cy, raw, protected) {
 
 // Draws a pixel to the canvas
 async function setPixel(r, g, b, x, y) {
-  var idx = (imageWidth * y + x) << 2;
+    let idx = (imageWidth * y + x) << 2;
 
-  canvas.data[idx + 0] = r;
-  canvas.data[idx + 1] = g;
-  canvas.data[idx + 2] = b;
-  canvas.data[idx + 3] = 255;
+    canvas.data[idx + 0] = r;
+    canvas.data[idx + 1] = g;
+    canvas.data[idx + 2] = b;
+    canvas.data[idx + 3] = 255;
 }
 
 // Timeout thingy
@@ -168,7 +166,7 @@ function setChunkDrawn(cx, cy) {
     bmap[(imageChunksV * cy) + cx] = false;
 }
 function checkAllDrawn() {
-    for (var i = 0; i < imageChunksTotal; i++) {
+    for (let i = 0; i < imageChunksTotal; i++) {
         if (bmap[i]) {
           return false;
         }
@@ -179,23 +177,6 @@ function checkAllDrawn() {
 
 // Help message
 function showUsage() {
-    console.log('USAGE:\n\tnode mapper.js [world_name] [x_pos] [y_pos] [height] [length]\n\tImages will be put in a "screenshots" folder');
+    console.log('USAGE:\n\tnode mapper.js [world_name] [x_pos] [y_pos] [height] [width]\n\tImages will be put in a "screenshots" folder');
     process.exit(1);
-}
-
-// Stupid int parser
-function parseArgsAsInts(argv, argc) {
-    var i = 0;
-    var ints = new Array(argc);
-
-    try {
-        for (; i < argc; ++i) {
-            ints[i] = Math.floor(Number(argv[i]));
-        }
-    } catch (e) {
-        console.error('Error: ' + argv[i] + ' is not a valid integer');
-        showUsage();
-    }
-
-    return ints;
 }
